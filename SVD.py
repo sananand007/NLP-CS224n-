84b5dea45a002235df2279ae3de1acc14b8c91fa
Sandeep Anand <anandjairam@ufl.edu>
2017-10-31 19:15:25 -0700

    Add files via upload
    Updates , Lecture 3 , CS224n

--- SVD.py
+++ SVD.py
@@ -3,12 +3,13 @@ Representing a Windows Based Concurrence Matrix
 Corpus: The quick brown fox jumped over the moon and was happy with that.
 - Make sure to convert everything to small
 - X is the cooccurence Matrix
+- Why don't we have the largest singular values for the 1st two columns here ??
 '''
 
 import numpy as np
 la =np.linalg
-corpus= "the quick brown fox jumped over the moon and he was happy with that."
-
+corpus= "the quick brown fox jumped over the quick moon and he was happy with that."
+#corpus= "i like deep learning i like nlp i enjoy flying."
 class concurrenceMatrix(object):
     """docstring for concurrenceMatrix."""
     def __init__(self):
@@ -27,35 +28,39 @@ class concurrenceMatrix(object):
         listofngrams = list(zip(*[words[i:] for i in range(type)]))
         return listofngrams
 
-    def arrayed(self,words):
+    def arrayed(self,words,uniquewords):
         #Form a N*N array , which represent bigrams only for now
         #initialize
-        N = len(words)
-        X = [[0]*N for i in range(N)]
+        N = len(uniquewords)
+        X = np.zeros(shape=(N,N))
         listofngrams = self.ngram(words, 2)
-        for idx, gram in enumerate(listofngrams):
-            centerword = gram[0]
-            for idx2,gram2 in enumerate(listofngrams):
-                if centerword==gram2[0]:
-                    idxnew = words.index(gram2[1])
-                    X[idx][idxnew]=1
+        mapwordtoindex = {}
+        for i in range(0,len(uniquewords)):
+            mapwordtoindex[uniquewords[i]] = i
+        for idx, word in enumerate(uniquewords):
+            for idx2,gram in enumerate(listofngrams):
+                if word==gram[0]:
+                    idxnew = mapwordtoindex[gram[1]]
+                    X[idx][idxnew]+=1
+                    X[idxnew][idx]=X[idx][idxnew]
         return X
 
 obj = concurrenceMatrix()
 words = obj.breakintoWords(corpus)
-X = np.asarray(obj.arrayed(words))
-#print(X)
+uniquewords = []
+for word in words:
+    if word not in uniquewords:
+        uniquewords.append(word)
+print(uniquewords)
+X = np.asarray(obj.arrayed(words,uniquewords))
+print(X)
+#a=np.random.normal(size=[10,8])
 U, S, Vh = la.svd(X, full_matrices=False)
-
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+val = pca.fit_transform(U)
 import matplotlib.pyplot as plt
-#plotting the cooccurence Matrix
 plt.figure()
-#print(U.shape, S.shape, Vh.shape)
-#print(U)
-for i in range(len(words)):
-    print(np.max(U[i,:]))
-    plt.text(U[i,0], U[i,1], words[i])
-#ax=plt.gca()
-#ax.set_ylim([0,0.05])
-#ax.set_xlim([0,0.03])
-#plt.show()
+for i in range(len(uniquewords)):
+    plt.text(val[i,0], val[i,1], uniquewords[i])
+plt.show()    
